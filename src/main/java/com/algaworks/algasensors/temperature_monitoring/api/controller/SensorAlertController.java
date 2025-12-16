db857d2c30d2d137134747bf31707fe5c568a1b3
pinto.breno <breno.pedro.a.pinto@accenture.com>
2025-12-16 14:14:38 -0300

    created endpoints for sensor alerts

---
+++ src/main/java/com/algaworks/algasensors/temperature_monitoring/api/controller/SensorAlertController.java
@@ -0,0 +1,59 @@
+package com.algaworks.algasensors.temperature_monitoring.api.controller;
+
+import com.algaworks.algasensors.temperature_monitoring.api.model.SensorAlertInput;
+import com.algaworks.algasensors.temperature_monitoring.api.model.SensorAlertOutput;
+import com.algaworks.algasensors.temperature_monitoring.domain.model.SensorAlert;
+import com.algaworks.algasensors.temperature_monitoring.domain.model.SensorId;
+import com.algaworks.algasensors.temperature_monitoring.domain.repository.SensorAlertRepository;
+import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/sensors/{sensorId}/alert")
+@RequiredArgsConstructor
+public class SensorAlertController {
+
+    private final SensorAlertRepository sensorAlertRepository;
+
+    @GetMapping
+    public SensorAlertOutput getSensorAlert(@PathVariable TSID sensorId) {
+        SensorAlert sensorAlert = sensorAlertRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        return toOutput(sensorAlert);
+    }
+
+    @PutMapping
+    public SensorAlertOutput updateSensorAlert(@PathVariable TSID sensorId,
+                                               @RequestBody SensorAlertInput input) {
+        SensorAlert sensorAlert = sensorAlertRepository.findById(new SensorId(sensorId))
+                .orElse(SensorAlert.builder()
+                        .id(new SensorId(sensorId))
+                        .build());
+
+        sensorAlert.setMaxTemperature(input.getMaxTemperature());
+        sensorAlert.setMinTemperature(input.getMinTemperature());
+
+        return toOutput(sensorAlertRepository.save(sensorAlert));
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteSensorAlert(@PathVariable TSID sensorId) {
+        SensorAlert sensorAlert = sensorAlertRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        sensorAlertRepository.delete(sensorAlert);
+    }
+
+    private SensorAlertOutput toOutput(SensorAlert sensorAlert) {
+        return SensorAlertOutput.builder()
+                .id(sensorAlert.getId().getValue())
+                .maxTemperature(sensorAlert.getMaxTemperature())
+                .minTemperature(sensorAlert.getMinTemperature())
+                .build();
+    }
+}
